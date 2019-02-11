e65fb9ca3ea32fdda525e4e9e859e4533d24a4dd
Bård Røtzer <bard@kartoteket.as>
2019-02-11 14:27:13 +0200

    Add a search view

---
+++ src/views/Search.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <ReactiveBase
+    app="products"
+    url="http://localhost:9200" >
+
+  <DataSearch
+    className="result-list-container"
+    componentId="CategorySearch"
+    :dataField="['name']"
+    categoryField="path"
+
+  />
+
+      <div>
+        {{searchresults}}
+      </div>
+
+    </ReactiveBase>
+  </div>
+</template>
+
+<script>
+// import { ReactiveBase, DataSearch } from '@appbaseio/reactivesearch-vue'
+
+export default {
+  name: 'search-component',
+  data() {
+    return {
+      searchresults: null,
+    }
+  },
+  // components: {
+  //   ReactiveBase,
+  //   DataSearch
+  // }
+
+}
+</script>
+
+<style>
+
+</style>
