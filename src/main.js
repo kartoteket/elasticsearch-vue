301c1adf26a69e844443ec06b26868eba7fe82d6
Bård Røtzer <bard@kartoteket.as>
2019-02-11 14:26:37 +0200

    hook up the search

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import ReactiveSearch from "@appbaseio/reactivesearch-vue";
 Vue.config.productionTip = false
+Vue.use(ReactiveSearch);
 
 new Vue({
   render: h => h(App),
